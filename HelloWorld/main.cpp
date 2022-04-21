0fe6fda8448f70f5cb5ddf61a0ef513f355d49f5
Adalmando <adalmando.silva@dcx.upfb.br>
2022-04-21 15:35:05 -0300

    Primeiro código, olá mundo

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World!\n";
+    cout << "E ai, Adalmando!\n";
+    cout << "Tudo bem com voce?\n";
+    system("pause");
+    return 0;
+}
